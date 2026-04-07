8a70cc122f461e697a1dffa0f1e5e4274023c2f1
jin woo sung <jinwoosung4507@gmail.com>
2026-04-07 21:26:03 +0530

    Added show task system

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-f = open("task.txt","a+")
-f.write(input("ADD TASK: "))
-f.close
+print("1. ADD TASK\n2. SHOW TASK\n3. EXIT")
+
+command = int(input("COMMAND: ").strip())
+
+if command == 1:
+    def add_task():
+        f = open("task.txt","+a")
+        task = input("WRITE TODAYS TASKS: ")
+        f.write("\n"+task)
+        f.close()
+    add_task()
+
+elif command == 2: 
+    def show_task():
+        f = open("task.txt","r")
+        data = f.read()
+        print(data)
+    show_task()
+
+
+
+
+
+
+
+
+
+
