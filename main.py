0f5e19674a707a75a8b99b735596a5f5aaac0623
jin woo sung <jinwoosung4507@gmail.com>
2026-04-11 20:39:21 +0530

    Added loop system in v1

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
-menu = print("1. ADD TASK\n2. SHOW TASK\n3. SHOW LEVEL\n4. ADD LEVEL\n5. EXIT")
+while True:
+    menu = print("1. ADD TASK\n2. SHOW TASK\n3. SHOW LEVEL\n4. ADD LEVEL\n5. EXIT")
 
-command = int(input("COMMAND: ").strip())
+    command = int(input("COMMAND: ").strip())
+          
+    if command == 1:
+        def add_task():
+            f = open("task.txt","+a")
+            task = input("WRITE TODAYS TASKS: ")
+            f.write("\n"+task)
+            f.close()
+        add_task()
 
-if command == 1:
-    def add_task():
-        f = open("task.txt","+a")
-        task = input("WRITE TODAYS TASKS: ")
-        f.write("\n"+task)
-        f.close()
-    add_task()
+    elif command == 2: 
+        def show_task():
+            f = open("task.txt","r")
+            data = f.read()
+            print(data)
+        show_task()
 
-elif command == 2: 
-    def show_task():
-        f = open("task.txt","r")
-        data = f.read()
-        print(data)
-    show_task()
+    elif command == 3:
+        def show_level():
+            f = open("level.txt","r")
+            data = f.read()
+            print(data)
+            f.close()
+        show_level()
 
-elif command == 3:
-    def show_level():
-        f = open("level.txt","r")
-        data = f.read()
-        print(data)
-        f.close()
-    show_level()
+    elif command == 4:
+        def add_level():
+            f = open("level.txt","w")
+            f.write(input("UPGRADE LEVEL: "))
+            f.close()
+        add_level()
 
-elif command == 4:
-    def add_level():
-        f = open("level.txt","w")
-        f.write(input("UPGRADE LEVEL: "))
-        f.close()
-    add_level()
+    else:
+        break
+    
+    
+    
+    
+    
 
 
 
