f95fb5ddaac398994e9905caf305a45a9ce12507
jin woo sung <jinwoosung4507@gmail.com>
2026-04-18 11:28:53 +0530

    added task delete system in v1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 while True:
-    menu = print("1. ADD TASK\n2. SHOW TASK\n3. SHOW LEVEL\n4. ADD LEVEL\n5. DELETE LEVEL\n6. EXIT")
+    menu = print("1. ADD TASK\n2. SHOW TASK\n3. SHOW LEVEL\n4. ADD LEVEL\n5. DELETE TASK\n6. EXIT")
 
     command = int(input("COMMAND: ").strip())
           
@@ -8,8 +8,9 @@ while True:
             f = open("task.txt","+a")
             task = input("WRITE TODAYS TASKS: ")
             task = task.upper().strip()
-            f.write("\n"+task).upper()
-            f.close()
+            f.write("\n"+task)
+            f.close()  
+        task = list  
         add_task()
 
     elif command == 2: 
@@ -36,13 +37,41 @@ while True:
 
     elif command == 5:
         def delete_task():
-            f = open("task.txt","w+")
-            f.write(input('DELETE TASK: '))
-            f.close()
+            with open("task.txt","r") as file: 
+                tasks = file.readlines()
+            if len(tasks) == 0: 
+                print("NO task available!")
+            else: 
+                print("\nYour tasks:")
+                for i in range(len(tasks)):
+                    print(f"{i+1}.{tasks[i].strip()}")
+                
+                try:
+                    choice =int(input("\nEnter task number to delete: "))
+
+                    if choice < 1 or choice > len(tasks):
+                        print("invalid choice")
+                    else:
+                        index = choice - 1 
+
+                        removed_task = tasks.pop(index)
+
+                        with open("tasks.txt","w") as file:
+                            for task in tasks:
+                                file.write(task)
+                        
+                        print(f"task'{removed_task.strip()}' deleted successfully ")
+                
+                except ValueError:
+                    print("please enter a valid number")
+                
         delete_task()
-    
+
+
     else:
         break
+
+
     
     
     
